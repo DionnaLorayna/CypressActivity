011732059f08cfe02e5c52aa5d17043a287bf994
DionnaLorayna <dionna.lorayna.s@southlandcollege.edu.ph>
2023-03-29 15:56:32 +0800

    Added new file

---
+++ cypress/e2e/Textbox.spec.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+
+context ('Texbox Test', () => {
+    it ('Test Case 1: Positive Text Test', () =>{
+        cy.visit ('https://demoqa.com/text-box');
+
+    });
+      
+});
