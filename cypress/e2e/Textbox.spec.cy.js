dc533dc69312482b6ce384b8f5a2467b6497467e
DionnaLorayna <dionna.lorayna.s@southlandcollege.edu.ph>
2023-03-30 12:57:37 +0800

    revised content

--- cypress/e2e/Textbox.spec.cy.js
+++ cypress/e2e/Textbox.spec.cy.js
@@ -1,10 +1,18 @@
 /// <reference types="cypress" />
-
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // prevent Cypress from failing the test
+    return false
+  })
+  
 
 context ('Texbox Test', () => {
     it ('Test Case 1: Positive Text Test', () =>{
         cy.visit ('https://demoqa.com/text-box');
-
+        cy.get('#userName').type('Dionna Lorayna');
+        cy.get('#userEmail').type('dio@gmail.com');
+        cy.get('#currentAddress').type('Zone 1, Baldoza, Lapaz, Iloilo City');
+        cy.get('#permanentAddress').type('Tabugon, Kabankalan City, Negros Occidental');
+        cy.get('#submit').click();
     });
       
 });
